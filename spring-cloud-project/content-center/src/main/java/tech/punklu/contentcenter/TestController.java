5eaf79564393af045161981f7a0f2f7876492a90
punk1u <punk1u@protonmail.com>
2021-01-27 22:55:23 +0800

    update

--- spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/TestController.java
+++ spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/TestController.java
@@ -1,6 +1,8 @@
 package tech.punklu.contentcenter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.punklu.contentcenter.dao.content.ShareMapper;
@@ -28,5 +30,21 @@ public class TestController {
         this.shareMapper.insertSelective(share);
 
         return this.shareMapper.selectAll();
+    }
+
+    /**
+     * DiscoveryClient对于Nacos、Zookeeper、eureka都适用
+     */
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 测试服务发现，证明内容中心可以找到用户中心
+     * @return 用户中心所有实例的地址信息
+     */
+    @GetMapping("/getInstances")
+    public List<ServiceInstance> getInstances(){
+        // 查询指定服务的所有实例的信息
+        return this.discoveryClient.getInstances("user-center");
     }
 }
