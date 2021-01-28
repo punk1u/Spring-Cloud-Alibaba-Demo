507f596e941784e6086b38455735f613ddf36838
punk1u <punk1u@protonmail.com>
2021-01-28 23:59:06 +0800

    comment code

--- spring-cloud-project/content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
+++ spring-cloud-project/content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RibbonConfiguration {
 
+    /**
+     * 自定义内容中心调用用户中心时的Ribbon负载均衡规则为随机
+     * @return
+     */
     @Bean
     public IRule ribbonRule(){
         return new RandomRule();
