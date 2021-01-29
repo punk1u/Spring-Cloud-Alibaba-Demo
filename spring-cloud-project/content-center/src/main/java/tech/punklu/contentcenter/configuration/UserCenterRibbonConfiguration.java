f7509eee67e6990ca24a0bed899f32c564f5e8af
punk1u <punk1u@protonmail.com>
2021-01-29 12:50:02 +0800

    change ribbon rule config from code config to config file

--- spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -3,8 +3,8 @@ package tech.punklu.contentcenter.configuration;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Configuration;
 import ribbonconfiguration.RibbonConfiguration;
-
+/*
 @Configuration
-@RibbonClient(name = "user-center",configuration = RibbonConfiguration.class)
+@RibbonClient(name = "user-center",configuration = RibbonConfiguration.class)*/
 public class UserCenterRibbonConfiguration {
 }
