1554b86f96b44778712fe2766aea9b62ad5539d9
punk1u <punk1u@protonmail.com>
2021-01-29 20:25:50 +0800

    use more way of the other Ribbon config

--- spring-cloud-project/content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
+++ spring-cloud-project/content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
@@ -1,9 +1,27 @@
 package ribbonconfiguration;
 
+import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
 import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Ribbon自定义规则不能在Spring Boot启动类所在的包下面，
+ * 而要在和Spring Boot启动类所在的包平级的包下面创建。层级关系如下：
+ * tech.punklu.contentcenter.configuration
+ * 	    ContentCenterApplication (Spring Boot启动类)
+ * ribbonconfiguration
+ * 	    RibbonConfiguration (自定义Ribbon负载均衡规则类)
+ *
+ * 原因在于Spring的父子上下文问题，如果将自定义的Ribbon负载均衡规则放到了和Spring Boot启动类同层级的目录下，
+ * 就会被Spring Boot扫描到自定义类上的`@Configuration`注解，
+ * 从而导致最后一个被扫描到的自定义Ribbon规则类被当成全局自定义负载均衡规则。
+ * 因此，为了实现细粒度的负载均衡规则，
+ * 必须确保自定义的Ribbon负载均衡规则类处于独立的子上下文中而不是Spring默认的父上下文中。
+ */
 
 @Configuration
 public class RibbonConfiguration {
@@ -15,5 +33,13 @@ public class RibbonConfiguration {
     @Bean
     public IRule ribbonRule(){
         return new RandomRule();
+    }
+
+    /**
+     * 自定义Ping的方式
+     */
+    @Bean
+    public IPing ping(){
+        return new PingUrl();
     }
 }
--- spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,10 +1,17 @@
 package tech.punklu.contentcenter.configuration;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Configuration;
 import ribbonconfiguration.RibbonConfiguration;
-/*
+
+/**
+ * 如果需要设置全局Ribbon负载均衡规则，只需要:
+ * 1、将@RibbonClient注解替换成@RibbonClients注解
+ * 2、将注解中的configuration属性设置为自定义的负载均衡规则类RibbonConfiguration即可
+ */
+
 @Configuration
-@RibbonClient(name = "user-center",configuration = RibbonConfiguration.class)*/
+@RibbonClient(name = "user-center",configuration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
 }
