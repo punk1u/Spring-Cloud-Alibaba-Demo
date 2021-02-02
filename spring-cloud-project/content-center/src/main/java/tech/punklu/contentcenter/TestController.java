b36b7bdfe2ae6f5770a9f2c5b8e39aa8afa79f84
punk1u <punk1u@protonmail.com>
2021-02-02 20:54:26 +0800

    使用@SentinelResource注解实现流控降级

--- spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/TestController.java
+++ spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/TestController.java
@@ -159,4 +159,39 @@ public class TestController {
             ContextUtil.exit();
         }
     }
+
+    @GetMapping("/test-sentinel-resource")
+    @SentinelResource(value = "test-sentinel-resource",
+            blockHandler = "block",
+            fallback = "fallback")
+    public String testSentinelResource(@RequestParam(required = false) String a){
+
+        // 被保护的业务逻辑
+        if (StringUtils.isBlank(a)){
+            throw new IllegalArgumentException("a cannot be blank");
+        }
+        return a;
+    }
+
+    /**
+     * 处理限流或降级
+     * @param a
+     * @param e
+     * @return
+     */
+    public String block( String a,BlockException e){
+        log.warn("限流，或者降级了 block",e);
+        return "限流，或者降级了 block";
+    }
+
+    /**
+     * 处理降级
+     * @param a
+     * @param e
+     * @return
+     */
+    public String fallback( String a,BlockException e){
+        log.warn("限流，或者降级了 fallback",e);
+        return "限流，或者降级了 fallback";
+    }
 }
