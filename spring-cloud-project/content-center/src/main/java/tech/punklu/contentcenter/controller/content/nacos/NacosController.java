01f1ff3606bcb567ac39d109b03e0b413a28dccf
punk1u <punk1u@protonmail.com>
2021-02-09 22:05:25 +0800

    添加注释，以及在shared-configs和extension-configs里指定要访问的配置项的group

--- spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/controller/content/nacos/NacosController.java
+++ spring-cloud-project/content-center/src/main/java/tech/punklu/contentcenter/controller/content/nacos/NacosController.java
@@ -1,13 +1,19 @@
 package tech.punklu.contentcenter.controller.content.nacos;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 不光需要在配置文件中自动刷新配置：refresh-enabled: true，
+ * 还需要在使用到Nacos配置信息的类上添加@RefreshScope注解才能实现自动更新
+ */
 @RestController
 @RequestMapping("/nacos")
+@RefreshScope
 public class NacosController {
 
 //    @Value("${nacosKey}")
